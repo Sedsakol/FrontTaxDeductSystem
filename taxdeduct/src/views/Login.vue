06fd98c95f2e409e77fec61c226bedb16d20021e
nnnumtan <60010878@kmitl.ac.th>
2020-09-04 01:45:23 +0700

    merge log-in and nav-bar component
    log-in component and nav-bar component

--- taxdeduct/src/views/Login.vue
+++ taxdeduct/src/views/Login.vue
@@ -1,12 +1,17 @@
 <template>
-    <Bar/>
+    <div>
+        <Bar/>
+        <LoginCard/>
+    </div>
 </template>
 <script>
 import Bar from "../components/Bar"
+import LoginCard from "../components/LoginCard"
 export default {
     name : 'Login',
     components : {
         Bar,
+        LoginCard,
     }
 }
 </script>
