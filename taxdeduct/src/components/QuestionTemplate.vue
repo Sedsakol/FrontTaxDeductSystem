0c7c443f06466ccbca4328f2fc63bfd8db885292
nnnumtan <60010878@kmitl.ac.th>
2020-11-08 17:25:37 +0700

    create question template.vue

---
+++ taxdeduct/src/components/QuestionTemplate.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="card">
+    <div class="d-flex justify-content-md-center"> 
+      <div class="card w-50 col-md-auto">
+        <div class="card-body">
+          <h4 class="text-center card-title mb-4 mt-1">(คำถามเพิ่มเติม ประกอบการแนะนำการลุงทุน)</h4>
+          <!-- Question -->
+          <slot name="question">Question</slot>
+          <!-- Choose -->
+          <slot name="choice"></slot>
+
+
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
