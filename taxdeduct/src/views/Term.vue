eb53454920cc91dd9c2d9b2fc2296098849dc2f8
nnnumtan <60010878@kmitl.ac.th>
2020-12-12 18:38:34 +0700

    adding Term Page

---
+++ taxdeduct/src/views/Term.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <TermCard/>
+    </div>
+</template>
+<script>
+import TermCard from "@/components/TermCard.vue"
+export default {
+    name : 'Term',
+    components : {
+        TermCard,
+    }
+}
+</script>
