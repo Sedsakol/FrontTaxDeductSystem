ce67e0e2d88d7985861fb3421f340602a4e7c342
Sedsakol Kamolchote <posezaza@gmail.com>
2020-12-19 07:25:50 +0700

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,11 @@ module.exports = {
         port: 8080 ,
         host: '0.0.0.0',        
         //public: '161.246.5.140:8080',
+        proxy: {
+            '^/' : {
+                target: 'http://161.246.5.140',
+                changeOrigin: true
+            },
+        }
     } 
 }
