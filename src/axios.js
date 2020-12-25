de4c8d9b76fc7d1a04f5c0ccc40da666319c2cb2
Sedsakol <sedsakol.ka@gmail.com>
2020-12-25 16:54:33 +0700

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 //axios.defaults.baseURL = 'http://161.246.5.140/';
-//axios.defaults.baseURL = 'https://taxdeduct.herokuapp.com/';
-axios.defaults.baseURL = 'http://localhost:8000/';
+axios.defaults.baseURL = 'https://taxdeduct.herokuapp.com/';
+//axios.defaults.baseURL = 'http://localhost:8000/';
