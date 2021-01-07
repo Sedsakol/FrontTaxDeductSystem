7d4e5217f7c626c0442731702fe26c93941ca778
nnnumtan <60010878@kmitl.ac.th>
2021-01-07 18:28:23 +0700

    update question page
    - แบบทดสอบชุดใหม่
    - เพิ่มหน้าผลลัพธ์
    - required ทุกข้อแล้วแต่ว่าข้อความไม่เด้งเตือน

--- src/components/QuestionTemplate.vue
+++ src/components/QuestionTemplate.vue
@@ -3,37 +3,42 @@
     <div class="d-flex justify-content-md-center">
       <div class="card w-50 col-md-auto">
         <div class="card-body">
-          <h4 class="text-center card-title mb-4 mt-1">แบบทดสอบระดับความเสี่ยงที่ยอมรับได้</h4>
-
-          <div v-for="(q, index) in question" :key="q.number">
+          <!-- start quiz -->
+          <form @submit.prevent = "submit" id="quiz" v-if="doingquiz">
+            <h4 class="text-center card-title mb-4 mt-1">แบบทดสอบระดับความเสี่ยงที่ยอมรับได้</h4>
             <!-- questionTitle -->
-            <h6>ข้อ {{index + 1}}. {{q.ask}}</h6>
+            <div v-for="(q, index) in question" :key="q.number">
+              <h6>ข้อ {{index + 1}}. {{q.ask}}</h6>
+              <!-- quizOptions -->
+              <b-form-group class="optionContainer">
+                <b-form-radio-group v-for="c in q.choice" :key="c.value" :id="'q'+ index">
+                  <input type="radio" 
+                    :name="'name' + index"
+                    :id="'id' + index + c.value" 
+                    v-model=" userResponses[index]"
+                    :value="c.value" 
+                    required />
+                  <label class="option" 
+                    :for="'id' + index + c.value" >
+                    {{ c.text }}
+                  </label>
+                </b-form-radio-group>
+              </b-form-group>
+            </div>
 
-            <!-- quizOptions -->
-            <b-form-group class="optionContainer">
-              <b-form-radio-group v-for="c in q.choice" :key="c.value" :id="'q'+ index">
-                <input type="radio" 
-                  :name="'name' + index"
-                  :id="'id' + index + c.value" 
-                  v-model=" userResponses[index]"
-                  :value="c.value" />
-                <label class="option" 
-                  :for="'id' + index + c.value" >
-                  {{ c.text }}
-                </label>
-              </b-form-radio-group>
-            </b-form-group>
-          </div>
+            <div class="d-flex justify-content-md-center">
+              <button @click="clear" type="button" class="btn btn-outline-primary" id="regularbutton">
+                  ล้าง
+              </button><div class="pr-4"/>
+              <button type="submit" class="btn btn-primary" id="regularbutton">
+                  คำนวณผล
+              </button>
+            </div>
 
-          <div class="d-flex justify-content-md-center">
-            <button @click="clear" type="button" class="btn btn-outline-primary" id="regularbutton">
-                ล้าง
-            </button><div class="pr-4"/>
-            <button @click="submit" class="btn btn-primary" id="regularbutton">
-                คำนวณผล
-            </button>
-          </div>
-
+          </form>
+          <!-- end quiz -->
+          
+          <!-- start modal -->
           <b-modal size="lg" ref="modal-condition" ok-title="ตกลง" title="การทำแบบสอบถามประเมินระดับความเสี่ยง" ok-only centered> 
             <div>
               แบบทดสอบระดับความเสี่ยงที่ยอมรับได้นี้เป็นเพียงการประเมินเบี้องต้นเท่านั้น ระดับความเสี่ยงที่ยอมรับได้จะแตกต่างกันไปตามลักษณะเฉพาะของแต่ละบุคคล
@@ -44,6 +49,25 @@
               </a>
             </div>
           </b-modal>
+          <!-- end modal -->
+
+          <!-- start result -->
+          <div id="quizresult" v-if="!doingquiz">
+            <h4 class="text-center card-title mb-4 mt-1">
+              คุณอยู่ใน <span>"ระยะ{{type}}"</span> และ <span>"ยอมรับความเสี่ยงได้{{quizresult[risk_level].text}}"</span>
+            </h4>
+            <h6>คำอธิบาย</h6>
+            <p class="pb-5">{{quizresult[risk_level].descrip}}</p>
+
+            <div class="d-flex justify-content-md-center">
+              <button @click="back" type="button" class="btn btn-outline-primary" id="regularbutton">
+                  กลับ
+              </button><div class="pr-4"/>
+              <button @click="restart" class="btn btn-primary" id="regularbutton">
+                  ทำใหม่อีกครั้ง
+              </button>
+            </div>
+          </div>
 
           
         </div>
@@ -61,6 +85,7 @@ export default {
   },
   data() {
     return {
+      doingquiz: true,
       userResponses: [
         null, //score1 Accumulator, Consolidator, Spender
         null, //score2
@@ -72,17 +97,24 @@ export default {
         null, //score8
         null, //score9
         null  //score10
-      // userResponses tag
+      ], 
+      type: '',
+      risk_level: '0',
+      quizresult: [
+        { text: 'น้อย', descrip: 'คุณชอบการลงทุนที่มีความปลอดภัยสูง ต้องการเห็นเงินต้นอยู่ครบ คุณควรกระจายการลงทุนไปยังสินทรัพย์ที่มีความผันผวนต่ำ แม้ว่าผลตอบแทนอาจจะไม่มาก แต่ก็รู้ค่อนข้างแน่นอนว่าจะได้ผลตอบแทนเท่าไหร่และเมื่อไหร่ คุ้มค่าและปลอดภัยกับเงินลงทุนของคุณ' },
+        { text: 'ปานกลาง', descrip: 'คุณยอมรับความเสี่ยงจากการลงทุนได้ปานกลาง ยอมรับความผันผวนได้ในระดับหนึ่งแต่ต้องไม่มากเกินไป เพื่อแลกกับผลตอบแทนที่มากกว่าเงินฝากธนาคารคุณควรกระจายการลุงทุนไปยังสินทรัพย์ที่ให้ได้ทั้งการเพิ่มมูลค่าในระยะยาวและกระแสเงินสดที่สม่ำเสมอจากการลงทุน' },
+        { text: 'สูง', descrip: 'คุณยอมรับความเสี่ยงจากกการลงทุนได้ระดับสูง ไม่กังวลกับความผันผวนที่จะเกิดขึ้นระหว่างการลงทุนเท่าไหร่ และมีโอกาสจะได้รับผลตอบแทนในระดับที่สูงขึ้นด้วย แต่อย่างไรก็ไม่ควรประมาท คุณอาจกระจายการลงทุนไปยังสินทรัพย์ที่มีการเพิ่มมูลค่าได้สูงในระยะยาว' },
       ],
     }
   },
-    computed: {
+  computed: {
 
   },
   methods: {
     clear() {
-        this.userResponses=[null, null, null, null, null, null, null, null, null];
-        // console.log(this.userResponses);
+      this.userResponses = [null, null, null, null, null, null, null, null, null, null];
+      this.type = '';
+      this.risk_level = '0';
     },
     submit() {
       this.$refs['modal-condition'].show()
@@ -95,20 +127,26 @@ export default {
       }
       //0 - 20
       if (score < 21) {
-        risk_level = 'low';
+        risk_level = '0';
       }
       //21 - 28
       else if (score < 29) {
-        risk_level = 'medium';
+        risk_level = '1';
       }
       //29++
       else {
-        risk_level = 'high';
+        risk_level = '2';
       }
-      console.log('score =' + score);
-      console.log('type =' + type);
-      console.log('risk_level =' + risk_level);
-      return type, risk_level;
+      this.type = type;
+      this.risk_level = risk_level;
+      this.doingquiz = false
+    },
+    back() {
+      this.doingquiz = true
+    },
+    restart() {
+      this.clear()
+      this.doingquiz = true
     },
     showModal() {
       this.$refs['modal-condition'].show()
