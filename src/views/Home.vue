47936dee13035c3d267163952e03d79b8586f210
nnnumtan <60010878@kmitl.ac.th>
2021-01-11 15:20:53 +0700

    edit image on home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,7 +4,7 @@
       <h4>เครื่องมือในการคำนวณภาษี และแนะนำการลงทุนเพื่อลดหย่อนภาษี</h4>
       <h6>เว็บแอพพลิเคชั่นที่จะช่วยวางแผนภาษี และเตรียมข้อมูลภาษีได้ง่ายขึ้น</h6>
       <h6>พร้อมเสนอแนวทางในการลงทุนเพื่อลดหย่อนภาษีให้ใกล้เคียงกับความต้องการ</h6>
-      <b-img center fluid alt="Responsive image" src="../assets/images/home2.svg"></b-img><p/>
+      <b-img center fluid alt="Responsive image" src="../assets/images/home.svg"></b-img><p/>
       <router-link to = "/taxCalculate">
         <button block class="btn btn-primary mt-3" id="regularbutton">เริ่มต้นคำนวณภาษี</button>
       </router-link>
