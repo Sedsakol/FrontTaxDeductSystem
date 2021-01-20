917dc1a245de40f90530bb8eb46a079b811f6c64
nnnumtan <60010878@kmitl.ac.th>
2021-01-21 00:15:37 +0700

    update css

---
+++ src/views/Profile2.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <ProfileCard2/>
+    </div>
+</template>
+<script>
+import ProfileCard2 from "@/components/ProfileCard2.vue";
+export default {
+    name : 'Profile2',
+    components : {
+        ProfileCard2,
+    }
+}
+</script>
+<style>
+
+</style>
