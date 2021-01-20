4b58654bcceeb97036bcedd30442ac36adc20a86
nnnumtan <60010878@kmitl.ac.th>
2021-01-21 00:21:02 +0700

    Revert "Revert "update css""
    This reverts commit 38470d0dc9635929f42bc7e5351e524908bebdbd.

---
+++ src/views/Question2.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <QuestionCard2/>
+    </div>
+</template>
+<script>
+import QuestionCard2 from "@/components/QuestionCard2.vue";
+export default {
+    name : 'Question2',
+    components : {
+        QuestionCard2,
+    }
+}
+</script>
+<style>
+
+</style>
