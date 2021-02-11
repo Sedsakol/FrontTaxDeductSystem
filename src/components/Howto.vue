79180ae5913189fa34441b6c7e20446c1cd5f73f
nnnumtan <60010878@kmitl.ac.th>
2021-02-12 00:53:10 +0700

    adding images on how to page

--- src/components/Howto.vue
+++ src/components/Howto.vue
@@ -3,11 +3,13 @@
     <div class="card">
       <div class="card-body">
         <h4 class="text-center card-title">ขั้นตอนการลบบัญชีผู้ใช้</h4>
-        <p>ขั้นตอนที่ 1 เข้าสู่ บัญชีของฉัน</p>
+        <b-img src="../assets/images/how_to_delete.svg"></b-img><p/>
+        <!-- <p>ขั้นตอนที่ 1 เข้าสู่ บัญชีของฉัน</p>
         <b-img src="../assets/images/delete_step1.svg"></b-img><p/>
         <p>ขั้นตอนที่ 2 เลือก <span class="text-subblue">ลบบัญชี</span></p>
         <b-img src="../assets/images/delete_step2.svg"></b-img><p/>
-        <p>ขั้นตอนที่ 3 เลือก <span class="text-danger">ลบ</span></p>
+        <p>ขั้นตอนที่ 3 เลือก <span class="text-danger">ลบ</span></p> 
+        <b-img src="../assets/images/delete_step3.svg"></b-img><p/> -->
         <p></p>
       </div>
     </div>
