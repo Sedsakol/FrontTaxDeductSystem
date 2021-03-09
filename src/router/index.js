82a30a1bf9cf79f7ab5e9f45a729461637fa0e94
nnnumtan <60010878@kmitl.ac.th>
2021-03-09 18:09:33 +0700

    update index.js
    go to top when scolling windows

--- src/router/index.js
+++ src/router/index.js
@@ -118,7 +118,11 @@ const routes = [
 const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
-  routes
+  routes,
+  scrollBehavior (to, from, savedPosition) {
+    // return desired position
+    return { x: 0, y: 0 }
+  }
 });
 
 export default router;
