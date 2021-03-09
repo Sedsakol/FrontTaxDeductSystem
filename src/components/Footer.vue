608c536f0ffb4829a96ac375992c357b6ecfd0f3
nnnumtan <60010878@kmitl.ac.th>
2021-03-10 04:38:09 +0700

    update footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,15 +3,15 @@
     <b-container>
     <b-row>
       <b-col>
-          <b>ข้อมูลเพิ่มเติม</b><p/>
-          <router-link to="/policy">นโยบายความเป็นส่วนตัว</router-link><p/>
-          <router-link to="/term">นโนบายและเงื่อนไขการใช้งาน</router-link><p/>
-          <router-link to="/how_to_delete">ขั้นตอนการลบบัญชี</router-link><p/>
+        <b>ข้อมูลเพิ่มเติม</b><p/>
+        <router-link to="/policy">นโยบายความเป็นส่วนตัว</router-link><p/>
+        <router-link to="/term">นโนบายและเงื่อนไขการใช้งาน</router-link><p/>
+        <router-link to="/how_to_delete">ขั้นตอนการลบบัญชี</router-link><p/>
       </b-col>
       <b-col>
-          <b>ช่องทางการติดต่อ</b><p/>
-          email : {{ email_url }}<p/>
-          our website : {{ our_website_url }}<p/>
+        <b>ช่องทางการติดต่อ</b><p/>
+        <p>email : <a :href="'mailto:' + email_url">{{ email_url }}</a></p>
+        <p>our website : <a :href="our_website_url">{{ our_website_url }}</a></p>
       </b-col>
     </b-row>
     </b-container>
